9a63bb65a820964c9ddf67ee6af6f6a8b8da49c7
ASUS-PC\Nick <bulletn58@gmail.com>
2017-05-26 00:29:10 +0300

    Add gitignore and project properties

---
+++ MainForm.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace _2048
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gameField = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblBestScore = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCurrentScore = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.назадToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.оИгреToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.gameField)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gameField
+            // 
+            this.gameField.Location = new System.Drawing.Point(12, 61);
+            this.gameField.Name = "gameField";
+            this.gameField.Size = new System.Drawing.Size(400, 400);
+            this.gameField.TabIndex = 1;
+            this.gameField.TabStop = false;
+            this.gameField.Paint += new System.Windows.Forms.PaintEventHandler(this.gameField_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DimGray;
+            this.panel1.Controls.Add(this.lblBestScore);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblCurrentScore);
+            this.panel1.Location = new System.Drawing.Point(12, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(400, 28);
+            this.panel1.TabIndex = 2;
+            // 
+            // lblBestScore
+            // 
+            this.lblBestScore.AutoSize = true;
+            this.lblBestScore.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblBestScore.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblBestScore.Location = new System.Drawing.Point(159, 3);
+            this.lblBestScore.Name = "lblBestScore";
+            this.lblBestScore.Size = new System.Drawing.Size(17, 19);
+            this.lblBestScore.TabIndex = 5;
+            this.lblBestScore.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.DimGray;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label2.Location = new System.Drawing.Point(113, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Best";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DimGray;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label1.Location = new System.Drawing.Point(3, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Score";
+            // 
+            // lblCurrentScore
+            // 
+            this.lblCurrentScore.AutoSize = true;
+            this.lblCurrentScore.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblCurrentScore.ForeColor = System.Drawing.SystemColors.Control;
+            this.lblCurrentScore.Location = new System.Drawing.Point(56, 3);
+            this.lblCurrentScore.Name = "lblCurrentScore";
+            this.lblCurrentScore.Size = new System.Drawing.Size(17, 19);
+            this.lblCurrentScore.TabIndex = 0;
+            this.lblCurrentScore.Text = "0";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.назадToolStripMenuItem,
+            this.оИгреToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(424, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // назадToolStripMenuItem
+            // 
+            this.назадToolStripMenuItem.Name = "назадToolStripMenuItem";
+            this.назадToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.назадToolStripMenuItem.Text = "Назад";
+            this.назадToolStripMenuItem.Click += new System.EventHandler(this.назадToolStripMenuItem_Click);
+            // 
+            // оИгреToolStripMenuItem
+            // 
+            this.оИгреToolStripMenuItem.Name = "оИгреToolStripMenuItem";
+            this.оИгреToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.оИгреToolStripMenuItem.Text = "О игре";
+            this.оИгреToolStripMenuItem.Click += new System.EventHandler(this.оИгреToolStripMenuItem_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 469);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.gameField);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.gameField)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox gameField;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblCurrentScore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblBestScore;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem назадToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem оИгреToolStripMenuItem;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2048
+{
+    public partial class MainForm : Form
+    {
+        private int count_step = 0;
+        private readonly Game game = new Game();
+        private readonly Bitmap bitmap;
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            if (gameField.Height != gameField.Width)
+            {
+                throw new InvalidOperationException("Game field must be quadratic.");
+            }
+            for (int row = 0; row < Game.FieldSize; row++)
+            {
+                for (int column = 0; column < Game.FieldSize; column++)
+                {
+                    game.Old_s[row, column] = game.Field[row, column];
+                    game.New_s[row, column] = game.Field[row, column];
+                }
+            }
+            bitmap = new Bitmap(gameField.Height, gameField.Width);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            drawGameField();
+        }
+
+        private void gameField_Paint(object sender, PaintEventArgs e)
+        {
+            gameField.BackgroundImage = bitmap;
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            bool isGameOver = false;
+
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    isGameOver = game.makeTurn(Direction.Up);
+                    count_step++;
+                    break;
+                case Keys.Down:
+                    isGameOver = game.makeTurn(Direction.Down);
+                    count_step++;
+                    break;
+                case Keys.Left:
+                    isGameOver = game.makeTurn(Direction.Left);
+                    count_step++;
+                    break;
+                case Keys.Right:
+                    isGameOver = game.makeTurn(Direction.Right);
+                    count_step++;
+                    break;
+            }
+
+            lblCurrentScore.Text = game.currentScore.ToString();
+
+            drawGameField();
+            if (isGameOver)
+            {
+                lblBestScore.Text = game.bestScore.ToString();
+                if (MessageBox.Show("Field will be cleaned.\n Your final score: "
+                    + game.currentScore.ToString(), "Game over", MessageBoxButtons.OK) == DialogResult.OK)
+                {
+                    game.restart();
+                    drawGameField();
+                }
+            }
+        }
+
+        private void drawGameField()
+        {
+            for (int row = 0; row < Game.FieldSize; row++)
+            {
+                for (int column = 0; column < Game.FieldSize; column++)
+                {
+                    int cellValue = game.Field[row, column];
+                    Point drawPosition = CellStyle.getDrawPosition(bitmap, row, column);
+                    drawCell(game.Field[row, column], drawPosition);
+                }
+            }
+            gameField.Refresh();
+        }
+
+        private void drawCell(int value, Point drawPosition)
+        {
+            Graphics graphics = Graphics.FromImage(bitmap);
+            int cellSize = CellStyle.getCellSize(bitmap);
+            Color color = CellStyle.getColor(value);
+            Brush brush = new SolidBrush(color);
+
+            graphics.FillRectangle(brush, drawPosition.X, drawPosition.Y, cellSize, cellSize);
+
+            if (value != 0)
+            {
+                drawCellValue(graphics, value, drawPosition);
+            }
+        }
+
+        private void drawCellValue(Graphics graphics, int value, Point drawPosition)
+        {
+            if (value == 0)
+            {
+                throw new UnsopportedCellValueException("Zero must not be displayed.");
+            }
+
+            Font font = CellStyle.getFont(value);
+            Point margin = CellStyle.getValueMargin(value);
+            Brush brush = new SolidBrush(Color.White);
+            float x = drawPosition.X + margin.X;
+            float y = drawPosition.Y + margin.Y;
+            graphics.DrawString(value.ToString(), font, brush, x, y);
+        }
+
+        private void назадToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (count_step >= 10)
+            {
+                for (int row = 0; row < Game.FieldSize; row++)
+                {
+                    for (int column = 0; column < Game.FieldSize; column++)
+                    {
+                        game.Field[row, column] = game.Old_s[row, column];
+                        game.New_s[row, column] = game.Old_s[row, column];
+                        int cellValue = game.Old_s[row, column];
+                        Point drawPosition = CellStyle.getDrawPosition(bitmap, row, column);
+                        drawCell(game.Old_s[row, column], drawPosition);
+                    }
+                }
+                count_step = 0;
+            }
+            else
+            {
+                int func_act_after = 10;
+                if (count_step != 10 && count_step != 0)
+                    MessageBox.Show("Данная функция станет доступна через: " + (func_act_after - count_step) + " ходов");
+            }
+            gameField.Refresh();
+        }
+
+        private void оИгреToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Help form2 = new Help();
+            form2.Show();
+        }
+    }
+}
+    
